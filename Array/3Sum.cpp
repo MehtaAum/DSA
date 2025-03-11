04c9d58ec211cc2a7076e27c6dc6624584e63876
Mehta Aum <maum5302@gmail.com>
2025-03-11 20:29:25 +0530

    Array

--- Array/3Sum.cpp
+++ Array/3Sum.cpp
@@ -1,3 +1,19 @@
+// 3 Sum :-
+
+//question from :- (coding ninja)
+
+// Problem statement :-
+
+// You are given an array/list ARR consisting of N integers. Your task is to find all the distinct triplets present in the array which adds up to a given number K.
+
+// An array is said to have a triplet {ARR[i], ARR[j], ARR[k]} with sum = 'K' if there exists three indices i, j and k such that i!=j, j!=k and i!=j and ARR[i] + ARR[j] + ARR[k] = 'K'.
+
+// Note:
+// 1. You can return the list of values in any order. For example, if a valid triplet is {1, 2, -3}, then {2, -3, 1}, {-3, 2, 1} etc is also valid triplet. Also, the ordering of different triplets can be random i.e if there are more than one valid triplets, you can return them in any order.
+// 2. The elements in the array need not be distinct.
+// 3. If no such triplet is present in the array, then return an empty list, and the output printed for such a test case will be "-1".
+
+
 //( both are right but 2 method take less time )
 
 //method : 1
